c35886e9cb6fc5e2f7a044a4e19e709b416c92d1
Your Name <you@example.com>
2026-06-15 19:16:55 +0530

    Multi stock downloader completed

--- ingestion.py/download_stock.py
+++ ingestion.py/download_stock.py
@@ -1,43 +1,32 @@
 import yfinance as yf
 import pandas as pd
-from prettytable import PrettyTable
 
-# Stock symbol
-symbol = "RELIANCE.NS"
+symbols = ["RELIANCE.NS", "TCS.NS", "INFY.NS", "HDFCBANK.NS"]
 
-# Download data
-df = yf.download(
-    symbol,
-    start="2020-01-01",
-    end="2025-01-01",
-    interval="1d"
-)
+for stock in symbols:
 
-# Create PrettyTable
-table = PrettyTable()
+    # Download stock data
+    df = yf.download(
+        stock,
+        start="2020-01-01",
+        end="2025-01-01",
+        interval="1d"
+    )
 
-# Add column names
-table.field_names = df.columns.tolist()
+    print(f"\nFirst 5 Rows for {stock}:\n")
+    print(df.head())
 
-# Add first 5 rows
-for row in df.head().values:
-    table.add_row(row)
+    print(f"\nData Information for {stock}:\n")
+    df.info()
 
-# Print nice table
-print("\nFirst 5 Rows of Stock Data:\n")
-print(table)
+    print(f"\nDataset Shape for {stock}:")
+    print(df.shape)
 
-# DataFrame information
-print("\nData Information:\n")
-df.info()
+    output_path = f"data/raw/{stock}.csv"
 
-# Shape
-print("\nDataset Shape:")
-print(df.shape)
+    df.to_csv(
+        output_path,
+        
+    )
 
-# Save CSV
-output_path = f"data/raw/{symbol}.csv"
-
-df.to_csv(output_path)
-
-print(f"\nData saved to: {output_path}")
+    print(f"\nData saved to: {output_path}")
